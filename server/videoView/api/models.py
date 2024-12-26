e9f977c77a0d88d631aee65416684322556a5b68
rushikadam12 <rushi55578@gmail.com>
2024-12-26 14:05:55 +0530

    init

--- server/videoView/api/models.py
+++ server/videoView/api/models.py
@@ -17,6 +17,7 @@ class Videos(models.Model):
         blank=False,
         null=False
     )
+    
     title=models.CharField(255)
     description=models.TextField()
     duration=models.IntegerField()
